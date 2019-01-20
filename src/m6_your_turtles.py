8cd9b7f89798047b31bee66a0f987997486df211
Yiqing Li <liy22@rose-hulman.edu>
2019-01-19 23:34:06 -0500

    All todo done.

--- src/m6_your_turtles.py
+++ src/m6_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
-         Aaron Wilkin, their colleagues, and PUT_YOUR_NAME_HERE.
+         Aaron Wilkin, their colleagues, and Yiqing Li.
 """
 ########################################################################
-# TODO: 1.
+# DONE: 1.
 #   On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# DONE: 2.
 #   You should have RUN the  m5e_loopy_turtles  module and READ its code.
 #   (Do so now if you have not already done so.)
 #
@@ -28,3 +28,37 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
 #   Don't forget to COMMIT-and-PUSH when you are done with this module.
 #
 ########################################################################
+
+import rosegraphics as rg
+
+window = rg.SimpleTurtle()
+
+pink_turtle = rg.SimpleTurtle('turtle')
+pink_turtle.pen = rg.Pen('pink',2)
+pink_turtle.speed = 30
+size = 200
+
+for k in range(10):
+    pink_turtle.draw_circle(size)
+
+    pink_turtle.pen_up()
+    pink_turtle.left(100)
+
+    pink_turtle.pen_down()
+    size = size - 10
+
+
+purple_turtle = rg.SimpleTurtle()
+purple_turtle.pen = rg.Pen('purple',3)
+purple_turtle.speed = 40
+length = 80
+
+
+for k in range(12):
+    purple_turtle.draw_regular_polygon(6,length)
+
+    purple_turtle.pen_up()
+    purple_turtle.left(150)
+
+    purple_turtle.pen_down()
+    length = length - 5
